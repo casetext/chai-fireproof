16de4deb1228302184bc3d90f33d93eddcd84406
Harry Schmidt <goldibex@casetext.com>
2014-11-10 14:09:23 -0800

    Gulp and build artifact

---
+++ dist/chai-fireproof.js
@@ -0,0 +1,481 @@
+
+(function (root, factory) {
+
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['chai'], factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(require('chai'));
+  } else {
+    // Browser globals (root is window)
+    root.chaiFireproof = factory(root.chai);
+  }
+
+}(this, function (chai) {
+
+  
+  // we have to hijack console.log to capture Firebase messages
+  chai.use(function() {
+  
+    chai.getFirebaseMessage = function(name) {
+      return chai._firebaseMsg[name];
+    };
+  
+    chai.flushFirebaseMessages = function() {
+  
+      chai._firebaseMsg = {
+        log: '',
+        warn: '',
+        error: ''
+      };
+  
+    };
+  
+    chai.flushFirebaseMessages();
+  
+    function hijackedLog(logFn, name) {
+  
+      return function() {
+  
+        var message = '';
+        for (var i = 0; i < arguments.length; i++) {
+          message += arguments[i].toString();
+        }
+  
+        if (message.match(/^FIREBASE.*?: /)) {
+          chai._firebaseMsg[name] += message.replace(/^FIREBASE.*?: /, '') + '\n';
+        } else {
+          logFn.apply(console, arguments);
+        }
+  
+      };
+  
+    }
+  
+  
+    if (console && console.log) {
+  
+      chai._firebaseLog = '';
+      chai._firebaseWarn = '';
+  
+      var oldLog = console.log,
+        oldWarn = console.warn,
+        oldError = console.error;
+  
+      console.log = hijackedLog(oldLog, 'log');
+      console.warn = hijackedLog(oldWarn, 'warn');
+      console.error = hijackedLog(oldError, 'error');
+  
+    }
+  
+  });
+  
+  
+  chai.use(function(chai, utils) {
+    // Overrides any Chai assertion in the list below to do the following:
+    // If the subject of the assertion is a Fireproof reference, the assertion
+    // will return a promise that resolves on passage of the test or rejects
+    // with the assertion error on failure.
+    function fireproofify(name, isProperty) {
+  
+      return function(_super) {
+  
+        return function() {
+          var oldArguments = arguments;
+          var self = this;
+          var obj = this._obj;
+          if (obj && obj.constructor.name === 'Fireproof') {
+            return obj.then(function(snap) {
+  
+              var newAssertion;
+  
+              if (name === 'satisfy') {
+                newAssertion = new chai.Assertion(snap);
+              } else if (utils.flag(self, 'priority')) {
+                newAssertion = new chai.Assertion(snap.getPriority());
+              } else {
+                newAssertion = new chai.Assertion(snap.val());
+              }
+  
+              utils.transferFlags(self, newAssertion, false);
+              newAssertion.message = self.message;
+  
+              if (isProperty) {
+  
+                // property.
+                newAssertion.to.be[name];
+  
+              } else {
+  
+                // method.
+                newAssertion.to[name].apply(newAssertion, oldArguments);
+  
+              }
+  
+            });
+  
+          } else {
+            _super.apply(this, arguments);
+          }
+  
+        };
+  
+      };
+  
+    }
+  
+    function fireproofifyChain() {
+  
+      return function(_super) {
+  
+        return function() {
+          return _super.apply(this, arguments);
+        };
+  
+      };
+  
+    }
+  
+    chai.Assertion.addChainableMethod('priority', function(expectedPriority) {
+  
+      if (this._obj && this._obj.constructor.name === 'Fireproof') {
+  
+        return this._obj.then(function(snap) {
+          new chai.Assertion(snap.getPriority()).to.equal(expectedPriority);
+        });
+  
+      } else {
+        throw new Error('Got a non-Fireproof object for priority call');
+      }
+  
+    }, function() {
+      utils.flag(this, 'priority', true);
+    });
+  
+  
+    chai.Assertion.addMethod('children', function(children) {
+  
+      if (this._obj && this._obj.constructor.name === 'Fireproof') {
+  
+        if (utils.flag(this, 'contains')) {
+  
+          return this._obj.then(function(snap) {
+            new chai.Assertion(snap.val()).to.contain.keys(children);
+          });
+  
+        } else {
+  
+          return this._obj.then(function(snap) {
+            new chai.Assertion(snap.val()).to.have.keys(children);
+          });
+  
+        }
+  
+      } else {
+        throw new Error('Got a non-Fireproof object for priority call');
+      }
+  
+    });
+  
+  
+    /* Some notes on behavior:
+     *
+     * "Satisfy" is different from all other methods insofar as it hands in
+     * the FireproofSnapshot itself rather than snap.val(), so you can do advanced
+     * testing on it.
+     *
+     * The following methods are new:
+     * - priority (chainable method), which checks the priority of the object
+     * - children(), which is an alias for keys()
+     *
+     * Since Firebase objects are always primitives, the following methods are
+     * not overridden:
+     * - instanceof (use "a/an" instead)
+     * - property/ownProperty/haveOwnProperty (use "include.key" instead)
+     * - empty (meaningless in Firebase, there are no empty arrays)
+     * - undefined (again meaningless in Firebase)
+     *
+     */
+    [
+      ['equal', 'method'],
+      ['equals', 'method'],
+      ['eq', 'method'],
+      ['eql', 'method'],
+      ['eqls', 'method'],
+      ['above', 'method'],
+      ['gt', 'method'],
+      ['greaterThan', 'method'],
+      ['least', 'method'],
+      ['gte', 'method'],
+      ['below', 'method'],
+      ['lt', 'method'],
+      ['lessThan', 'method'],
+      ['most', 'method'],
+      ['lte', 'method'],
+      ['within', 'method'],
+      ['lengthOf', 'method'],
+      ['length', 'chainableMethod'],
+      ['match', 'method'],
+      ['string', 'method'],
+      ['include', 'chainableMethod'],
+      ['contain', 'chainableMethod'],
+      ['keys', 'method'],
+      ['key', 'method'],
+      ['satisfy', 'method'],
+      ['closeTo', 'method'],
+      ['members', 'method'],
+      ['a', 'chainableMethod'],
+      ['an', 'chainableMethod'],
+      ['null', 'property'],
+      ['ok', 'property'],
+      ['true', 'property'],
+      ['false', 'property'],
+      ['exist', 'property']
+    ].forEach(function(kind) {
+  
+      switch(kind[1]) {
+      case 'method':
+        chai.Assertion.overwriteMethod(kind[0], fireproofify(kind[0]));
+        break;
+      case 'chainableMethod':
+        chai.Assertion.overwriteChainableMethod(kind[0], fireproofify(kind[0]), fireproofifyChain(kind[0]));
+        break;
+      default:
+        chai.Assertion.overwriteProperty(kind[0], fireproofify(kind[0], true));
+        break;
+      }
+  
+    });
+  
+  });
+  
+  
+  
+  chai.setFirebaseAuthToken = function(newToken) {
+    chai._firebaseToken = newToken;
+  };
+  
+  chai.FirebaseError = function FirebaseError(message) {
+  
+    Error.call(this);
+    this.message = message;
+  
+  };
+  
+  chai.FirebaseError.prototype = Object.create(Error.prototype);
+  
+  chai.use(function(chai, utils) {
+  
+    /*
+    CryptoJS v3.1.2
+    code.google.com/p/crypto-js
+    (c) 2009-2013 by Jeff Mott. All rights reserved.
+    code.google.com/p/crypto-js/wiki/License
+    */
+    var CryptoJS;
+    /* jshint ignore:start */
+    CryptoJS=function(h,s){var f={},g=f.lib={},q=function(){},m=g.Base={extend:function(a){q.prototype=this;var c=new q;a&&c.mixIn(a);c.hasOwnProperty("init")||(c.init=function(){c.$super.init.apply(this,arguments)});c.init.prototype=c;c.$super=this;return c},create:function(){var a=this.extend();a.init.apply(a,arguments);return a},init:function(){},mixIn:function(a){for(var c in a)a.hasOwnProperty(c)&&(this[c]=a[c]);a.hasOwnProperty("toString")&&(this.toString=a.toString)},clone:function(){return this.init.prototype.extend(this)}},
+    r=g.WordArray=m.extend({init:function(a,c){a=this.words=a||[];this.sigBytes=c!=s?c:4*a.length},toString:function(a){return(a||k).stringify(this)},concat:function(a){var c=this.words,d=a.words,b=this.sigBytes;a=a.sigBytes;this.clamp();if(b%4)for(var e=0;e<a;e++)c[b+e>>>2]|=(d[e>>>2]>>>24-8*(e%4)&255)<<24-8*((b+e)%4);else if(65535<d.length)for(e=0;e<a;e+=4)c[b+e>>>2]=d[e>>>2];else c.push.apply(c,d);this.sigBytes+=a;return this},clamp:function(){var a=this.words,c=this.sigBytes;a[c>>>2]&=4294967295<<
+    32-8*(c%4);a.length=h.ceil(c/4)},clone:function(){var a=m.clone.call(this);a.words=this.words.slice(0);return a},random:function(a){for(var c=[],d=0;d<a;d+=4)c.push(4294967296*h.random()|0);return new r.init(c,a)}}),l=f.enc={},k=l.Hex={stringify:function(a){var c=a.words;a=a.sigBytes;for(var d=[],b=0;b<a;b++){var e=c[b>>>2]>>>24-8*(b%4)&255;d.push((e>>>4).toString(16));d.push((e&15).toString(16))}return d.join("")},parse:function(a){for(var c=a.length,d=[],b=0;b<c;b+=2)d[b>>>3]|=parseInt(a.substr(b,
+    2),16)<<24-4*(b%8);return new r.init(d,c/2)}},n=l.Latin1={stringify:function(a){var c=a.words;a=a.sigBytes;for(var d=[],b=0;b<a;b++)d.push(String.fromCharCode(c[b>>>2]>>>24-8*(b%4)&255));return d.join("")},parse:function(a){for(var c=a.length,d=[],b=0;b<c;b++)d[b>>>2]|=(a.charCodeAt(b)&255)<<24-8*(b%4);return new r.init(d,c)}},j=l.Utf8={stringify:function(a){try{return decodeURIComponent(escape(n.stringify(a)))}catch(c){throw Error("Malformed UTF-8 data");}},parse:function(a){return n.parse(unescape(encodeURIComponent(a)))}},
+    u=g.BufferedBlockAlgorithm=m.extend({reset:function(){this._data=new r.init;this._nDataBytes=0},_append:function(a){"string"==typeof a&&(a=j.parse(a));this._data.concat(a);this._nDataBytes+=a.sigBytes},_process:function(a){var c=this._data,d=c.words,b=c.sigBytes,e=this.blockSize,f=b/(4*e),f=a?h.ceil(f):h.max((f|0)-this._minBufferSize,0);a=f*e;b=h.min(4*a,b);if(a){for(var g=0;g<a;g+=e)this._doProcessBlock(d,g);g=d.splice(0,a);c.sigBytes-=b}return new r.init(g,b)},clone:function(){var a=m.clone.call(this);
+    a._data=this._data.clone();return a},_minBufferSize:0});g.Hasher=u.extend({cfg:m.extend(),init:function(a){this.cfg=this.cfg.extend(a);this.reset()},reset:function(){u.reset.call(this);this._doReset()},update:function(a){this._append(a);this._process();return this},finalize:function(a){a&&this._append(a);return this._doFinalize()},blockSize:16,_createHelper:function(a){return function(c,d){return(new a.init(d)).finalize(c)}},_createHmacHelper:function(a){return function(c,d){return(new t.HMAC.init(a,
+    d)).finalize(c)}}});var t=f.algo={};return f}(Math);
+    (function(h){for(var s=CryptoJS,f=s.lib,g=f.WordArray,q=f.Hasher,f=s.algo,m=[],r=[],l=function(a){return 4294967296*(a-(a|0))|0},k=2,n=0;64>n;){var j;a:{j=k;for(var u=h.sqrt(j),t=2;t<=u;t++)if(!(j%t)){j=!1;break a}j=!0}j&&(8>n&&(m[n]=l(h.pow(k,0.5))),r[n]=l(h.pow(k,1/3)),n++);k++}var a=[],f=f.SHA256=q.extend({_doReset:function(){this._hash=new g.init(m.slice(0))},_doProcessBlock:function(c,d){for(var b=this._hash.words,e=b[0],f=b[1],g=b[2],j=b[3],h=b[4],m=b[5],n=b[6],q=b[7],p=0;64>p;p++){if(16>p)a[p]=
+    c[d+p]|0;else{var k=a[p-15],l=a[p-2];a[p]=((k<<25|k>>>7)^(k<<14|k>>>18)^k>>>3)+a[p-7]+((l<<15|l>>>17)^(l<<13|l>>>19)^l>>>10)+a[p-16]}k=q+((h<<26|h>>>6)^(h<<21|h>>>11)^(h<<7|h>>>25))+(h&m^~h&n)+r[p]+a[p];l=((e<<30|e>>>2)^(e<<19|e>>>13)^(e<<10|e>>>22))+(e&f^e&g^f&g);q=n;n=m;m=h;h=j+k|0;j=g;g=f;f=e;e=k+l|0}b[0]=b[0]+e|0;b[1]=b[1]+f|0;b[2]=b[2]+g|0;b[3]=b[3]+j|0;b[4]=b[4]+h|0;b[5]=b[5]+m|0;b[6]=b[6]+n|0;b[7]=b[7]+q|0},_doFinalize:function(){var a=this._data,d=a.words,b=8*this._nDataBytes,e=8*a.sigBytes;
+    d[e>>>5]|=128<<24-e%32;d[(e+64>>>9<<4)+14]=h.floor(b/4294967296);d[(e+64>>>9<<4)+15]=b;a.sigBytes=4*d.length;this._process();return this._hash},clone:function(){var a=q.clone.call(this);a._hash=this._hash.clone();return a}});s.SHA256=q._createHelper(f);s.HmacSHA256=q._createHmacHelper(f)})(Math);
+    (function(){var h=CryptoJS,s=h.enc.Utf8;h.algo.HMAC=h.lib.Base.extend({init:function(f,g){f=this._hasher=new f.init;"string"==typeof g&&(g=s.parse(g));var h=f.blockSize,m=4*h;g.sigBytes>m&&(g=f.finalize(g));g.clamp();for(var r=this._oKey=g.clone(),l=this._iKey=g.clone(),k=r.words,n=l.words,j=0;j<h;j++)k[j]^=1549556828,n[j]^=909522486;r.sigBytes=l.sigBytes=m;this.reset()},reset:function(){var f=this._hasher;f.reset();f.update(this._iKey)},update:function(f){this._hasher.update(f);return this},finalize:function(f){var g=
+    this._hasher;f=g.finalize(f);g.reset();return g.finalize(this._oKey.clone().concat(f))}})})();
+    /* jshint ignore:end */
+  
+    var codex = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  
+    function toBase64(str) {
+      /* jshint bitwise:false */
+  
+      var bits = [];
+      while(str.length > 0) {
+        bits.push(str.slice(0, 3));
+        str = str.slice(3);
+      }
+  
+      return bits
+      .reduce(function(b64, word) {
+  
+        var intVal = (word.charCodeAt(0) << 16) +
+          ( (word.charCodeAt(1) << 8) || 0) +
+          ( word.charCodeAt(2) || 0);
+  
+        return b64 + codex.charAt(intVal >> 18) +
+          codex.charAt( (intVal & 0x3F000) >> 12) +
+          (word.length > 1 ? codex.charAt( (intVal & 0xFC0) >> 6 ) : '' ) +
+          (word.length > 2 ? codex.charAt( (intVal & 0x3F)) : '' );
+  
+      }, '');
+  
+    }
+  
+    function createToken(obj, secret) {
+  
+      var header = toBase64(JSON.stringify({ alg: 'HS256', typ: 'JWT' }));
+      var payload = toBase64(JSON.stringify({
+        v: 0,
+        debug: true,
+        admin: obj && obj.admin === true,
+        iat: Math.round(Date.now() / 1000),
+        d: obj
+      }));
+      var message = header + '.' + payload;
+      var signature = CryptoJS.HmacSHA256(message, secret);
+  
+      return message + '.' + toBase64(signature.toString(CryptoJS.enc.Latin1));
+  
+    }
+  
+    chai.Assertion.addProperty('can', function() {
+      utils.flag(this, 'positivity', true);
+    });
+  
+    chai.Assertion.addProperty('cannot', function() {
+      utils.flag(this, 'positivity', false);
+    });
+  
+    chai.Assertion.addProperty('read', function() {
+      utils.flag(this, 'operation', 'read');
+    });
+  
+    chai.Assertion.addChainableMethod('write', function(data) {
+  
+      utils.flag(this, 'operation', 'write');
+      if (data) {
+        utils.flag(this, 'operationData', data);
+      }
+  
+    }, function() {
+      utils.flag(this, 'operation', 'write');
+    });
+  
+    chai.Assertion.addMethod('ref', function(ref) {
+  
+      var self = this,
+        users = self._obj,
+        operation = utils.flag(self, 'operation'),
+        data = utils.flag(self, 'operationData');
+  
+      if (!chai._firebaseToken) {
+        throw new chai.FirebaseError('You must call chai.setFirebaseAuthToken()!');
+      }
+  
+      if (typeof ref.unauth !== 'function') {
+        throw new chai.FirebaseError('You must hand in a ref to ref(), instead you handed in ' + ref);
+      }
+  
+      function promiseResolved(user) {
+  
+        return function() {
+  
+          var info = chai.getFirebaseMessage('log');
+          chai.flushFirebaseMessages();
+  
+          self.assert(
+            utils.flag(self, 'positivity') === true,
+            'Expected ' + (user ? user.uid : 'an unauthenticated user') +
+              ' not to be able to ' + operation +
+              (data ? JSON.stringify(data, undefined, 2) : '') +
+              ' at ref ' + ref.toString() +
+              ', but Firebase allowed it. Debug info:\n' + info,
+            ''
+          );
+  
+          return info;
+  
+        };
+  
+      }
+  
+      function promiseRejected(user) {
+  
+        return function(err) {
+  
+          var info = chai.getFirebaseMessage('log');
+          chai.flushFirebaseMessages();
+  
+          self.assert(
+            utils.flag(self, 'positivity') === false,
+            'Expected ' + (user ? user.uid : 'an unauthenticated user') +
+              ' to be able to ' + operation +
+              (data ? JSON.stringify(data, undefined, 2) : '') +
+              ' at ref ' + ref.toString() +
+              ', but received error:\n "' + err.message + '". Debug info:\n' +
+              info,
+            ''
+          );
+  
+          return info;
+  
+        };
+  
+      }
+  
+      function nextPromise(user) {
+  
+        return function() {
+  
+          var promise;
+  
+          if (user) {
+  
+            ref.unauth();
+            promise = ref.authWithCustomToken(createToken(user, chai._firebaseToken))
+            .then(function() {
+  
+              if (operation === 'read') {
+                return ref.once('value').then(promiseResolved(user), promiseRejected(user));
+              } else if (operation === 'write') {
+                return ref.set(data || null).then(promiseResolved(user), promiseRejected(user));
+              }
+  
+            });
+  
+          } else {
+  
+            ref.unauth();
+            if (operation === 'read') {
+              promise = ref.once('value').then(promiseResolved(user), promiseRejected(user));
+            } else if (operation === 'write') {
+              promise = ref.set(data || null).then(promiseResolved(user), promiseRejected(user));
+            }
+  
+          }
+  
+          return promise;
+  
+        };
+  
+      }
+  
+  
+      if (!(Array.isArray(users))) {
+        users = [users];
+      }
+  
+      return users.reduce(function(promise, user) {
+  
+        if (promise) {
+          return promise.then(nextPromise(user));
+        } else {
+  
+        }
+  
+      }, nextPromise(users[0])());
+  
+    });
+  
+  });
+  
+  
+
+}));
---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+
+'use strict';
+
+var rmdir = require('rimraf'),
+  gulp = require('gulp-help')(require('gulp')),
+  rename = require('gulp-rename'),
+  mocha = require('gulp-mocha'),
+  concat = require('gulp-concat'),
+  bump = require('gulp-bump'),
+  wrap = require('gulp-wrap'),
+  jsdoc2md = require('gulp-jsdoc-to-markdown'),
+  filter = require('gulp-filter'),
+  git = require('gulp-git'),
+  tagVersion = require('gulp-tag-version'),
+  version = require('./package.json').version,
+  Fireproof = require('fireproof'),
+  gutil = require('gulp-util');
+
+
+Fireproof.bless(require('q'));
+
+function npmPublish(done) {
+
+  return function() {
+
+    var spawn = require('child_process').spawn;
+
+    spawn('npm', ['publish'], { stdio: 'inherit' })
+    .on('error', done)
+    .on('close', done);
+
+  };
+
+}
+
+function inc(importance, done) {
+
+  return gulp.src(['./package.json'])
+  .pipe(bump({type: importance}))
+  .pipe(gulp.dest('./'))
+  .pipe(git.commit('new release'))
+  .pipe(filter('package.json'))
+  .pipe(tagVersion())
+  .on('end', function() {
+
+    git.push('origin', 'master', function(err) {
+      if (err) {
+        throw err;
+      }
+      npmPublish(done);
+    });
+
+  });
+
+}
+
+
+gulp.task('clean', 'Remove all build files', function(done) {
+  rmdir('./dist', done);
+});
+
+
+gulp.task('build', 'Builds the Javascript for distribution.', ['clean'], function() {
+
+  return gulp.src([
+    'lib/log.js',
+    'lib/assertion-extensions.js',
+    'lib/security.js'
+    ])
+  .pipe(concat('chai-fireproof.js'))
+  .pipe(wrap({ src: 'umd.template' }, { version: version }))
+  .pipe(gulp.dest('./dist'));
+
+});
+
+
+gulp.task('docs', 'Generates a new version of the docs.', ['build'], function() {
+
+  return gulp.src(['dist/chai-fireproof.js'])
+  .pipe(jsdoc2md())
+  .pipe(rename(function(path) {
+    path.basename = 'api';
+    path.extname = '.md';
+  }))
+  .pipe(gulp.dest('./'));
+
+});
+
+
+gulp.task('test:setup', 'Set up tests.', ['build'], function() {
+
+
+  var Firebase = require('firebase'),
+    chai = require('chai');
+
+  require('./dist/chai-fireproof');
+  global.chai = chai;
+  global.expect = chai.expect;
+
+  if (!process.env.FIREBASE_TEST_URL || !process.env.FIREBASE_TEST_SECRET) {
+
+    gutil.log('Please set FIREBASE_TEST_URL and FIREBASE_TEST_SECRET.');
+    process.exit(1);
+
+  }
+
+  global.root = new Fireproof(new Firebase(process.env.FIREBASE_TEST_URL));
+  global.authToken = process.env.FIREBASE_TEST_SECRET;
+
+});
+
+gulp.task('test', 'Runs tests and exits.', ['test:setup'], function() {
+
+  function testsDone(e) {
+    process.exit(e ? 1 : 0);
+  }
+
+  var tests;
+  if (process.env.ONLY) {
+    tests = './test/spec/' + process.env.ONLY + '.js';
+  } else {
+    tests = './test/spec/**/*.js';
+  }
+
+  return gulp.src(tests, { read: false })
+  .pipe(mocha({ slow: 1000, timeout: 5000 }))
+  .on('error', testsDone)
+  .on('end', testsDone);
+
+});
+
+var bumpDeps = ['test'];
+
+gulp.task('bump', 'Publishes a new bugfix version.', bumpDeps, function(done) {
+  inc('patch', done);
+});
+
+
+gulp.task('bump:minor', 'Publishes a new minor version.', bumpDeps, function(done) {
+  inc('minor', done);
+});
+
+
+gulp.task('bump:major', 'Publishes a new major version.', bumpDeps, function(done) {
+  inc('major', done);
+});
+
